79d5861ef0d62fed0e96e7fa16b905bb013de3e3
ame <shachi10969@gmail.com>
2022-10-04 21:22:56 +0900

    Initial Commit

---
+++ Remind/RemindApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindApp.swift
+//  Remind
+//
+//  Created by t&a on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct RemindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
